07b8d153fd745bb71de4e0fa5390b1460397df8b
SantiagoJaraHernandez <167146795+SantiagoJaraHernandez@users.noreply.github.com>
2025-06-15 23:43:34 -0500

    Fixing Eslint Errors

--- src/components/Destinos.tsx
+++ src/components/Destinos.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import Modal from "react-modal";
